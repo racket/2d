5fbd4c77d2c9cb5403748305a2218dab66fac6cd
Vincent St-Amour <stamourv@racket-lang.org>
2015-09-07 18:06:30 -0500

    Move tests from unstable.

---
+++ 2d-test/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection "2d")
+(define version "1.0")
+(define deps '("base" "2d-lib"))
+(define build-deps '("rackunit-lib" "unstable-options-lib"))
+(define pkg-desc "tests for \"2d\"")
+(define pkg-authors '(robby))
