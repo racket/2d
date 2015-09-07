f9f17bd5415547fe7917163859262b0b9ed11cd9
Vincent St-Amour <stamourv@racket-lang.org>
2015-09-07 18:20:58 -0500

    Add 2d meta-package.

---
+++ 2d/info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define collection 'multi)
+(define version "1.0")
+(define deps '("2d-lib" "2d-doc"))
+(define implies '("2d-lib" "2d-doc"))
+(define pkg-desc "2d syntax")
+(define pkg-authors '(robby))
