db8d11d37cf3ff282a63cec44e4cf032eb043418
Vincent St-Amour <stamourv@racket-lang.org>
2015-09-07 17:49:11 -0500

    Move docs from unstable docs.

---
+++ 2d-doc/info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define collection "2d")
+(define version "1.0")
+(define deps '("base" "2d-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define pkg-desc "Documentation part of \"2d\"")
+(define pkg-authors '(robby))
+
+(define scribblings '(("scribblings/2d.scrbl" () (library))))
