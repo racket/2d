0797516617cad7b8ef4e87926314319c008ee85f
Alex Knauth <alexander@knauth.org>
2016-08-19 10:23:34 -0400

    use `lang-reader-module-paths` instead of copy-pasted code
    Relies on https://github.com/racket/racket/commit/42dcc525b1e03ca36488b815b45428655fdcbf1b

--- 2d-lib/info.rkt
+++ 2d-lib/info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "2d")
 (define version "1.1")
-(define deps '("base"
+(define deps '(["base" #:version "6.6.0.3"]
                "scribble-lib"
                "syntax-color-lib"))
 (define pkg-desc "Implementation (no documentation) part of \"2d\"")
